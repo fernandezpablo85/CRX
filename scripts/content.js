721597c5b0f472ea3cbe27b5e3289f673c45d556
Pablo Fernandez <fernandezpablo85@gmail.com>
2010-02-01 20:33:58 -0300

    Added LI icon to mailtos

--- scripts/content.js
+++ scripts/content.js
@@ -1,18 +1,5 @@
-function fillProfiles(){}
-
 var $emails = $('a[href^=mailto:]');
 
-function createAuthForms(auth_url){
-  $wrapper = $('<span/>').addClass('licrx_wrap').hover(function(){
-    alert('over');
-  },function(){
-    alert('out');
-  });
-  
-  $emails.each(function(){
-    $(this).wrap($wrapper);
-  });
-}
 var emails = [];
 $emails.each(function(){
   emails.push(this.href);
@@ -26,4 +13,32 @@ if(emails.length > 0){
       createAuthForms(response.auth);
     }
   });
+}
+
+function createAuthForms(auth_url){
+  $wrapper = $('<span/>').addClass('li_ext_wrap');
+  $wrapper.hover(function(event){
+    if(event.currentTarget == this){
+      $(this).children('a.li_ext_icon').fadeIn('slow');
+    }
+  },function(event){
+    if(event.currentTarget == this){
+      $(this).children('a.li_ext_icon').fadeOut(600);
+    }
+  })
+  $emails.each(function(){
+    $(this).wrap($wrapper);
+  });
+  $anchor = $('<a/>').attr('href','http://www.linkedin.com').
+                      addClass('li_ext_icon').
+                      css({'text-decoration':'none','display':'none'});
+  
+  $icon = $('<img/>').attr('src',chrome.extension.getURL('img/icon.png')).
+                      css({'vertical-align':'middle','padding-left':'5px'});
+  $anchor.append($icon);
+  $('.li_ext_wrap').append($anchor);
+}
+
+function fillProfiles(){
+  
 }
